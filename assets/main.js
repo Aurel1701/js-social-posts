452a490f74c697b30ee1b68d6d08f04a5602f35f
Aurel1701 <gantzfan96@gmail.com>
2022-04-12 17:15:20 +0200

    edit card

--- assets/main.js
+++ assets/main.js
@@ -51,43 +51,72 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=534",
         "author": {
             "name": "Mariano Di Vaio",
-            "image": null
+            "image": ""
         },
         "likes": 43,
         "created": "03-09-2021"
     },
-    {
-        "id": 6,
-        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Michele Morrone",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 83,
-        "created": "03-09-2021"
-    },
-    {
-        "id": 7,
-        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Capo Plaza",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 4,
-        "created": "03-09-2021"
-    },
-    {
-        "id": 8,
-        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Cristiano Ronaldo",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 333,
-        "created": "03-11-2021"
-    },
+  
   
 ];
+
+
+
+const homepage = document.querySelector(".lista-post");
+
+for (let index = 0; index < posts.length; index++) {
+  homepage.insertAdjacentHTML('beforeend', `<div class="post">
+  <div class="post-header">
+      <div class="cnt1">
+          <div class="icon-cnt1">
+              <img class="img-cnt1" src= "${posts[index].media}" alt="Phil Mangione">
+          </div>
+          <div class="info-cnt1">
+              <div class="name-cnt1">${posts[index].author.name}</div>
+              <div class="date-cnt1">${posts[index].created}</div>
+          </div>
+      </div>
+  </div>
+  <div class="text-post">${posts[index].content}</div>
+  <img class="first-image" src="${posts[index].author.image}" alt="">
+  <div>
+      <div class="likes">
+          <div>
+                  <button id="button-like">Mi Piace</button>
+          </div>
+          <div>
+              Piace a <b>${posts[index].likes}</b> persone
+          </div>
+      </div>
+  </div>
+</div>
+
+</div>
+
+
+</div>
+  
+  `
+
+  )
+    
+}
+
+document.addEventListener("click", function(event){
+    console.log(event.target);
+ if (event.target.id === "button-like") {
+     console.log(event.child);
+
+     
+ }
+
+
+
+
+});
+
+
+
+
+
+
