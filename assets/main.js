a082cefc31c0b6d36801d27c8824fbab23409cf8
Aurel1701 <gantzfan96@gmail.com>
2022-04-12 15:05:20 +0200

    create Milestone 1

---
+++ assets/main.js
@@ -0,0 +1,93 @@
+//Milestone 1
+//Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+const posts = [
+    {
+        "id": 1,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 790,
+        "created": "25-06-2021"
+    },
+    {
+        "id": 2,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 402,
+        "created": "03-09-2021"
+    },
+    {
+        "id": 3,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Fedez",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 93,
+        "created": "03-09-2021"
+    },
+    {
+        "id": 4,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Chiara Ferragni",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 51,
+        "created": "03-09-2021"
+    },
+    {
+        "id": 5,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Mariano Di Vaio",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 43,
+        "created": "03-09-2021"
+    },
+    {
+        "id": 6,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Michele Morrone",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 83,
+        "created": "03-09-2021"
+    },
+    {
+        "id": 7,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Capo Plaza",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 4,
+        "created": "03-09-2021"
+    },
+    {
+        "id": 8,
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci possimus animi quasi inventore omnis, quae quidem tempora dignissimos neque.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Cristiano Ronaldo",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 333,
+        "created": "03-11-2021"
+    },
+  
+];
